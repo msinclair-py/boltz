1c4e0e8bc2d37c43a8e94475b84670bc57978a78
msinclair-py <msinclair01940@gmail.com>
2026-02-10 15:53:46 -0600

    fixed fairscale removal bug

--- src/boltz/model/modules/utils.py
+++ src/boltz/model/modules/utils.py
@@ -25,8 +25,8 @@ class MeanMetric(Module):
 
     def __init__(self) -> None:
         super().__init__()
-        self.register_buffer("weighted_sum", torch.tensor(0.0))
-        self.register_buffer("total_weight", torch.tensor(0.0))
+        self.register_buffer("weighted_sum", torch.tensor(0.0), persistent=False)
+        self.register_buffer("total_weight", torch.tensor(0.0), persistent=False)
 
     def update(self, value: torch.Tensor, weight: torch.Tensor = torch.tensor(1.0)) -> None:
         self.weighted_sum += (value * weight).sum()
